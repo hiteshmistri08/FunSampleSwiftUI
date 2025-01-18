6a446c596fef68573e2e1effb78816e31113f36d
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2025-01-18 11:31:51 +0530

    - Create About Me App

---
+++ AboutMeApp/AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Hitesh on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
