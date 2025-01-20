80f73cefb165794b2b37211b94700b3b9723d727
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2025-01-20 21:25:14 +0530

    - Create new app target "DatePlanner"

---
+++ AboutMeApp/DatePlannerApp/DatePlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerAppApp.swift
+//  DatePlannerApp
+//
+//  Created by Hitesh on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
